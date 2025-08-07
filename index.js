43456b8fae93f3af84d160641ce2886d052137f1
Cameron Smith <cameron.smith3030@gmail.com>
2025-08-06 22:32:15 -0400

    working flavor counter function, may try to polish more

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ let flavors = prompt("Enter froyo flavors (comma-separated): ");
 let flavorsArray = flavors.split(",");
 
 // console.log("Froyo Flavors Array: ", flavorsArray);
+
+function count(array) {
+  let flavorsCount = {};
+  for (let i = 0; i < array.length; i++) {
+    if (flavorsCount[array[i]]) {
+      flavorsCount[array[i]]++;
+    } else {
+      flavorsCount[array[i]] = 1;
+    }
+  }
+  return flavorsCount;
+}
+
+let flavorsCount = count(flavorsArray);
+
+console.log("How Many of Each Flavor You Ordered: ", flavorsCount);
