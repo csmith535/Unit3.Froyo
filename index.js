85571059582cd2bad0e49269c645c9d2dfdbe1d3
Cameron Smith <cameron.smith3030@gmail.com>
2025-08-06 22:23:42 -0400

    working steps 1-4 of the README.md

---
+++ index.js
@@ -0,0 +1,8 @@
+let flavors = prompt("Enter froyo flavors (comma-separated): ");
+
+// console.log("Froyo Flavors String: ", flavors);
+
+// Convert the input string to an array
+let flavorsArray = flavors.split(",");
+
+// console.log("Froyo Flavors Array: ", flavorsArray);
